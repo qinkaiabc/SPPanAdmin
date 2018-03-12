3b2e694393600ae04c9fe19f02cb281c8e51a0b3
whoismy8023 <whoismy8023@163.com>
2018-03-12 14:59:52 +0800

    fix bug

--- src/main/java/net/sppan/base/config/shiro/ShiroConfig.java
+++ src/main/java/net/sppan/base/config/shiro/ShiroConfig.java
@@ -1,11 +1,6 @@
 package net.sppan.base.config.shiro;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.annotation.Resource;
-
+import net.sppan.base.service.IResourceService;
 import org.apache.shiro.cache.CacheManager;
 import org.apache.shiro.cache.MemoryConstrainedCacheManager;
 import org.apache.shiro.mgt.DefaultSecurityManager;
@@ -18,7 +13,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
 
-import net.sppan.base.service.IResourceService;
+import javax.annotation.Resource;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @Import(ShiroManager.class)
@@ -63,7 +61,7 @@ public class ShiroConfig {
         shiroFilter.setLoginUrl("/admin/login");
         shiroFilter.setSuccessUrl("/admin/index");
         shiroFilter.setUnauthorizedUrl("/previlige/no");
-        Map<String, String> filterChainDefinitionMap = new HashMap<String, String>();
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
         filterChainDefinitionMap.put("/assets/**", "anon");
 
         filterChainDefinitionMap.put("/admin/login", "anon");
